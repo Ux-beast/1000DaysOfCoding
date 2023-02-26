f2b81682048b50fcab531ca68f25f79701afcc3c
Ux-Beast <mosendab2010@gmail.com>
2023-02-26 21:32:02 +0200

    functions for operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,45 @@
 const defaultResult =0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10)*3/2-1;
-let calculationDescription = `(${defaultResult} +10)* 3 / 2-1`;
-let errorMassage = 'An Error \n' + 'Occured';
-outputResult(calculationDescription,'');
+function getUserNumberIput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator,resultBeforeCalc,calcNumber){
+const calcDescription=`${resultBeforeCalc} ${operator} ${calcNumber} `;
+outputResult(currentResult,calcDescription);
+}
+
+function add(){
+    const enteredNumber = getUserNumberIput();
+    const initialResult = currentResult;
+    currentResult = currentResult+enteredNumber;
+    createAndWriteOutput('+',initialResult,enteredNumber);
+}
+
+function sub(){
+    const enteredNumber = getUserNumberIput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-',initialResult,enteredNumber);
+}
+
+function mult(){
+    const enteredNumber = getUserNumberIput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*',initialResult,enteredNumber);
+}
+
+function div(){
+    const enteredNumber = getUserNumberIput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/',initialResult,enteredNumber);
+}
+
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',sub)
+multiplyBtn.addEventListener('click',mult)
+divideBtn.addEventListener('click',div)
